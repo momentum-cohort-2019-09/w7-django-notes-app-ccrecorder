77629edfaf7f606f87485a07a685f61f4816ba48
Charles Wines <ccrecorder@gmail.com>
2019-10-24 13:56:59 -0400

    makes edit button

--- notes/views.py
+++ notes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from notes.models import Note
 from notes.forms import NoteForm
 
@@ -23,3 +23,18 @@ def new_note(request):
         form = NoteForm()    
 
     return render (request, "notes/new_note.html", {"form": form})
+
+def notes_edit(request, pk):
+    note = get_object_or_404(note, pk=pk)
+    if request.method = 'POST':
+        form = NoteForm(instance=note, data=request.POST)
+        if form.is_valid():
+            note = form.save()
+            return redirect(to=note)
+    else:
+        form = NoteForm(instance=note)
+
+    return render(request, 'notes/notes_edit.html', {
+        "note": note,
+        "form": form,
+    }
